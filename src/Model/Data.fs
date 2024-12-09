3793d6b66ab9ea1fa9f228b79e2b65f9ce7cc049
Onur Gumus <onur@outlook.com.tr>
2024-12-09 13:31:41 +0000

    Co-authored-by: Kurt Mueller <kurtronaldmueller@gmail.com> Co-authored-by: Nat Elkins <NatElkins@users.noreply.github.com> Co-authored-by: Asadbek Sindarov <asindarov@users.noreply.github.com>

---
+++ src/Model/Data.fs
@@ -0,0 +1,23 @@
+﻿module rec Banking.Model.Data
+
+open FCQRS.Model
+open FCQRS.Model.Data
+open FCQRS.Model.Aether
+
+type UserIdentity =
+    private UserIdentity of ShortString
+        static member Value_ : Lens<UserIdentity,ShortString> = 
+            (fun (UserIdentity u) -> u),
+            (fun (g: ShortString) _ ->   g |> UserIdentity)
+
+        override this.ToString() =
+            (ValueLens.Value this).ToString()
+
+type AccountName =
+    private AccountName of ShortString
+        static member Value_ : Lens<AccountName,ShortString> = 
+            (fun (AccountName u) -> u), 
+            (fun (g: ShortString) _ ->   g |> AccountName)
+
+        override this.ToString() = 
+            (ValueLens.Value this).ToString()
