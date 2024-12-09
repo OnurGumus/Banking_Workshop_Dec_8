82bd3a8d101933a8330f07ced826a6e91dba50a2
Onur Gumus <onur@outlook.com.tr>
2024-12-09 15:57:48 +0000

    Program.fs
    Co-authored-by: Kurt Mueller <kurtronaldmueller@gmail.com>
    Co-authored-by: Asadbek Sindarov <asindarov@users.noreply.github.com>

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -1,2 +1,39 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module Banking.Server.Program
+open Environments
+open Banking.Model.Data
+open Banking.Application.Command
+open Banking.Application.Command
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Logging
+open Banking.Application.Command.Accounting
+open System
+open FCQRS.Model.Data
+
+let configBuilder =  ConfigurationBuilder()
+let config = configBuilder.Build()
+let lf = LoggerFactory.Create(fun builder -> builder.AddConsole().AddDebug() |> ignore)
+
+
+let appEnv :AppEnv = new AppEnv(config,lf)
+
+appEnv.Reset()
+
+let accounting = appEnv :> IAccounting
+
+let cid: CID = Guid.NewGuid() |> string |> ValueLens.CreateAsResult |> Result.value
+
+let money: Money = ValueLens.Create 10
+
+let userIdentity: UserIdentity = "my user" |> ValueLens.CreateAsResult |> Result.value
+
+let accountName : AccountName =  "Account10" |> ValueLens.CreateAsResult |> Result.value
+
+let positiveMoney :PositiveMoney = money |> ValueLens.TryCreate |> Result.value
+
+let operationDetails = { UserIdentity = userIdentity; AccountName = accountName ; Money = positiveMoney} 
+
+let deposit = accounting.Deposit
+
+let accountingDeposit = deposit cid operationDetails |> Async.RunSynchronously
+
+printfn "%A" accountingDeposit
